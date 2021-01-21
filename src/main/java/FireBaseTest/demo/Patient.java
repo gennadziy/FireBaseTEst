bae6653fa5df3e94914d48c422acd70f84fe6ec7
geniy@tut.by <251284251284>
2021-01-21 09:54:49 +0100

    Initial commit

---
+++ src/main/java/FireBaseTest/demo/Patient.java
@@ -0,0 +1,19 @@
+package FireBaseTest.demo;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Patient {
+
+    private String name;
+
+    private int age;
+
+    private String city;
+}
+
+
